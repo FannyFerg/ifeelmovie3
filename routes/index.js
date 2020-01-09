d7811628e7671e6a323670e98e3d0ef2e9189fee
FannyFerg <53188359+FannyFerg@users.noreply.github.com>
2020-01-09 21:33:26 +0100

    v0

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/movie.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(movies => {
+      res.render('movies', {movies: movies});
+    })
+    .catch(next)
+  ;
+});
 
 
 
